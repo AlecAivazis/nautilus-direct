8a97f050e5f8c4ba1a44920fdea64ec96a1332cb
Alec Aivazis <alec@aivazis.com>
2016-06-24 00:37:43 -0700

    added service mixin

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nautilus-registry',
-    version='0.1.0',
+    version='0.1.2',
     description='A module for powering a nautilus application with a service registry.',
     author='Alec Aivazis',
     author_email='alec@aivazis.com',
